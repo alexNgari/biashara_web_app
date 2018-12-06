cec3e0078e0b4b710e9029e3631caf6e37c43776
Ngari <alex.ngari03@gmail.com>
2018-12-06 17:56:05 +0300

    more tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -10,11 +10,11 @@ def test_login_logout(test_client, init_database):
                                 data=dict(username='Lmutu', password='Lmutu'),
                                 follow_redirects=True)
     assert response.status_code == 200
-    assert b"Lmutu" in response.data
-    # assert b"Register Business" in response.data
-    # assert b"Choose" in response.data
-    # assert b"Log out" in response.data
-    # assert b"Log in" not in response.data
+    # assert b"Lmutu" in response.data
+    assert b"Register Business" in response.data
+    assert b"Choose" in response.data
+    assert b"Log out" in response.data
+    assert b"Log in" not in response.data
     
     # #########test logout functionality
     # response = test_client.get('/logout', follow_redirects=True)
