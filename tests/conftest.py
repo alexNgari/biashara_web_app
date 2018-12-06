9dc34619cb8fd07b6c274009ba70775a5b11d7c7
Ngari <alex.ngari03@gmail.com>
2018-12-06 11:58:01 +0300

    tests for models

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,33 @@
 import pytest
 from app import app
+from app.models import User, Business, Review
 
+
+################    MODELS  ####################
+@pytest.fixture(scope='module')
+def new_user():
+    user = User(first_name='Laurene', middle_name='Ngoya', last_name='Mutundu', username='Lmutu', email='lmutu@mail.com')
+    user.set_password('Lmutu')
+    return user
+
+@pytest.fixture(scope='module')
+def new_business():
+    business = Business(name='testBus', category='testCat', desctiption='testDesc', location='testLoc')
+    return business
+
+@pytest.fixture(scope='module')
+def new_review():
+    review = Review(user_id=999, business_id=999, post='post')
+    return review
+
+
+##############  FUNCTIONAL TESTS    #########################
 @pytest.fixture(scope='module')
 def test_client():
     app.config['TESTING'] = True
     app_client = app.test_client()
-
-    # return app_client     #not cool
     ctx = app.app_context()
     ctx.push()
     yield app_client
     ctx.pop()
+
