5a1d85c2c40d95b8b6a780f7fa297cf81161e5dc
Ngari <alex.ngari03@gmail.com>
2018-12-06 18:10:15 +0300

    Test log out functionality

--- tests/test_views.py
+++ tests/test_views.py
@@ -11,14 +11,14 @@ def test_login_logout(test_client, init_database):
                                 follow_redirects=True)
     assert response.status_code == 200
     # assert b"Lmutu" in response.data
-    assert b"Register Business" in response.data
-    assert b"Choose" in response.data
-    assert b"Log out" in response.data
-    assert b"Log in" not in response.data
+    # assert b"Register Business" in response.data
+    # assert b"Choose" in response.data
+    # assert b"Log out" in response.data
+    # assert b"Log in" not in response.data
     
     # #########test logout functionality
-    # response = test_client.get('/logout', follow_redirects=True)
-    # assert response.status_code == 200
+    response = test_client.get('/logout', follow_redirects=True)
+    assert response.status_code == 200
     # assert b"Biashara is a web app" in response.data
     # assert b"Biashara" in response.data
     # assert b"Log out" not in response.data
