ac2ad61b230ec6dd7874090d288824e3c04ef648
Ngari <alex.ngari03@gmail.com>
2018-12-06 23:44:47 +0300

    switched to unittest

---
+++ tests.py
@@ -0,0 +1,31 @@
+from app import app
+import unittest
+
+class FlaskTestCase(unittest.TestCase):
+    # Ensure flask works
+    def test_home(self):
+        test_client = app.test_client(self)
+        response = test_client.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    # Check home page load
+    def test_signup_load(self):
+        test_client = app.test_client(self)
+        response = test_client.get('/', content_type='html/text')
+        self.assertTrue(b'Biashara is a web app' in response.data)
+
+    # Ensure login behaves correctly given correct credentials
+    def test_correct_login(self):
+        test_client = app.test_client()
+        response = test_client.post('/login',
+                    data=dict(username='Lmutu', password='Lmutu'),
+                    follow_redirects=True)
+        self.assertIn(b'Reviews', response.data)
+
+
+
+    # Enssure login behaves correctly given wrong credentials    
+
+
+if __name__ == '__main__':
+    unittest.main()
