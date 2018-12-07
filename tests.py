d080053a270abd237f7961e05bb136980043b794
Ngari <alex.ngari03@gmail.com>
2018-12-07 19:35:49 +0300

    added more tests

--- tests.py
+++ tests.py
@@ -29,13 +29,33 @@ class FlaskTestCase(BaseTestCase):
         self.assert200
 
     # Check home page load
-    def test_signup_load(self):
+    def test_home_load(self):
         response = self.client.get('/', content_type='html/text')
         self.assertTrue(b'Biashara is a web app' in response.data)
 
-        # Ensure logout requires login
+    # Ensure logout requires login
     def test_logout_require_login(self):
         response = self.client.get('/logout', follow_redirects=True)
+        self.assert401
+
+    # Ensure landing page requires login
+    def test_landing_require_login(self):
+        response = self.client.get('/landing/Bloomy')
+        self.assert401
+
+    # Test signup method
+    def test_signup(self):
+        response = self.client.get('/sign_up')
+        self.assert200
+    
+    # Test signup page loads correctly
+    def test_signup_load(self):
+        response = self.client.get('/sign_up')
+        self.assertIn(b'Sign up', response.data)
+
+    # Test register business require login
+    def test_register_business_require_login(self):
+        response = self.client.get('/register_business/Bloomy', follow_redirects=True)
         self.assert401
 
         
@@ -46,7 +66,6 @@ class UsersViewsTests(BaseTestCase):
             response = self.client.post('/login',
                         data=dict(username='Bloomy', password='bloomy'),
                         follow_redirects=True)
-            self.assertIn(b'Reviews', response.data)
             self.assertTrue(current_user.username == 'Bloomy')
             self.assertTrue(current_user.is_active)
 
@@ -57,12 +76,43 @@ class UsersViewsTests(BaseTestCase):
                     follow_redirects=True)
         self.assertIn(b'Wrong username or password', response.data)
 
-    # Ensure landing page requires login
-    def test_landing_require_login(self):
-        response = self.client.get('/landing/Bloomy')
-        self.assert401
+    # Test logout user
+    def test_logout(self):
+        with self.client:
+            self.client.post('/login',
+                        data=dict(username='Bloomy', password='bloomy'))
+            response = self.client.get('logout', follow_redirects=True)
+            self.assertIn(b'Biashara is a web app', response.data)
 
+    # Test landing loads correctly for logged in user
+    def test_landing(self):
+        with self.client:
+            response = self.client.post('/login',
+                        data=dict(username='Bloomy', password='bloomy'),
+                        follow_redirects=True)
+            self.assertIn(b'Reviews', response.data)
 
+    # Test valid signup
+    def test_signup_functionality(self):
+        with self.client:
+            response = self.client.post('/sign_up', data=dict(first_name='Michael', middle_name='Bay', last_name='Musonda', username='mbay', email='mbay@mail.com', password='mbay', password2='mbay'),
+            follow_redirects=True)
+            self.assertIn(b'Sign-up Successful. Click login above to log in', response.data)
+
+    # Test reused username signup
+    def test_used_username_signup(self):
+        with self.client:
+            response = self.client.post('/sign_up', data=dict(first_name='Michael', middle_name='Bay', last_name='Musonda', username='Bloomy', email='mbay@mail.com', password='mbay', password2='mbay'),
+                follow_redirects=True)
+            self.assertIn(b'Username has already been taken', response.data)
+
+    # Test register business functionality
+    def test_register_business_functionality(self):
+        with self.client:
+            self.client.post('/login', data=dict(username='Bloomy', password='bloomy')) # log in user
+            response = self.client.post('/register_business/Bloomy', 
+                        data=dict(name='testName', category='testCat', description='testDesc', location='Area 51'))
+            self.assertIn(b'The business has been successfully registered', response.data)
 
 
 if __name__ == '__main__':
